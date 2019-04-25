edde9525a1b418e5adf2bf199d2f85f2c9e67d2b
Matthew Lollar <mjlollar1@gmail.com>
2019-04-24 20:38:07 -0500

    Update viability_null_final.py

--- viability_null_final.py
+++ viability_null_final.py
@@ -1,3 +1,5 @@
+# Python3.x compatibale 
+
 import sys
 import csv
 import scipy.stats as sp
@@ -5,9 +7,10 @@ import itertools as it
 
 csv.register_dialect('tab_delim', delimiter="\t", quoting=csv.QUOTE_NONE)
 
+#Input arguments taken
 file_name = sys.argv[1] 
 number_sequenced = int(sys.argv[2])
-number_sequenced = int(number_sequenced + number_sequenced)
+number_sequenced2 = int(number_sequenced*2)
 
 # function to enumerate rows
 def read_lines(csv_reader, row_list):
@@ -15,10 +18,7 @@ def read_lines(csv_reader, row_list):
 		if row_number in row_list:
 			yield row_number, row
 
-def drop(mylist, n):
-   del mylist[0::n]
-
-#functions to delete within-chromosome comparisons
+#functions used in deletion of within-chromosome comparisons
 def myrange1():
 	for value in reversed(range(401,500)):
 		yield value
@@ -29,19 +29,30 @@ def myrange3():
 	for value in reversed(range(1,202)):
 		yield value
 
+# Use every other fly in text file, increases "randomness" of selection
+# although I am not sure if this really matters. Does decrease null p-values in some cases.
+def drop(mylist, n):
+	del mylist[::n]
+
+
 with open(file_name, 'r') as File:
 	reader = csv.reader(File, dialect='tab_delim')
-	r = list(range(0, number_sequenced))
+	r = list(range(0, number_sequenced2))
 	
-	# Generate tuples of all pairwise window combinations and add to master list
+	# Generate tuples of all pairwise window combinations and add to master list "comparisons"
+	# Nested list, nested lists are indidivduals 
 	comparisons = []
 	for row_number, row in read_lines(reader, r):
 		row_tuples = list(it.combinations(row, 2))
 		comparisons.append(row_tuples)
 
 	drop(comparisons, 2)
-	
+
 	# Delete all within-chromosome comparisons
+	# 500c2 yields 124750 pairwise comparisons. Tuple generation works
+	# by comparing window1-window2[0], window1-window3[1], .... window1-window500[499], window2-window3[500], window2-window4[501], etc.
+	# Determined the index ranges that corresponds to comparisons within each chromosome and put them in list
+	# X-chromosome comparisons
 	first_value = 0
 	second_value = 98
 	set1 = []
@@ -51,6 +62,7 @@ with open(file_name, 'r') as File:
 		first_value = first_value + value
 		second_value = second_value + value - 1
 
+	#2nd chromosome comparisons
 	third_value = 44149
 	fourth_value = 44349
 	set2 = []
@@ -60,6 +72,7 @@ with open(file_name, 'r') as File:
 		third_value = third_value + value
 		fourth_value = fourth_value + value - 1
 
+	#3rd chromosome comparisons
 	fifth_value = 104449
 	sixth_value = 104649
 	set3 = []
@@ -69,15 +82,19 @@ with open(file_name, 'r') as File:
 		fifth_value = fifth_value + value
 		sixth_value = sixth_value + value - 1	
 
+	#Combine list of all index values corresponding to within chromosome comparisons and reverse order
+	#Reversal necessary so that iteration doesn't disrupt tuple index
 	combined_sets = set1 + set2 + set3
 	combined_sets = list((list(it.chain(*combined_sets))))
 	combined_sets = list(set(combined_sets))
 	combined_sets = sorted(combined_sets, reverse=True)
 
+	#Iterate through nested list to remove all within chromosome comparisons
 	for i in range(0,number_sequenced):
 		for value in combined_sets:
 			del comparisons[i][value]
 
+	#Rearange nested list so that each new nested list contains all sampled individuals for a given window 
 	window_list_comparisons = list(map(list, zip(*comparisons)))
 
 	#calculate p-values for each window
@@ -90,19 +107,28 @@ with open(file_name, 'r') as File:
 		for individual in window:
 			if individual==('0', '2'):
 				fishergroup1.append(1)
-			elif individual==('1', '2') or individual==('2', '2'):
+			elif individual==('1', '2'):
+				fishergroup2.append(1) 
+			elif individual==('2', '2'):
 				fishergroup2.append(1)
-			elif individual==('0', '0') or individual==('0', '1'):
+			elif individual==('0', '0'):
 				fishergroup3.append(1)
-			elif individual==('1', '0') or individual==('1', '1'): 
+			elif individual==('0', '1'):
+				fishergroup3.append(1)
+			elif individual==('1', '0'):
 				fishergroup4.append(1)
-			elif individual==('2', '0') or individual==('2', '1'):
+			elif individual==('1', '1'): 
+				fishergroup4.append(1)
+			elif individual==('2', '0'):
+				fishergroup4.append(1)
+			elif individual==('2', '1'):
 				fishergroup4.append(1)
 			else:
 				raise Exception("There is something wrong with your window values")
 		oddsratio, pvalue = sp.fisher_exact([[len(fishergroup1), len(fishergroup2)], [len(fishergroup3), len(fishergroup4)]])
 		p_values.append(pvalue)
 
+	#Take minimum pvalue from all windows and print it
 	lowest_pvalue = min(p_values)
 	print(lowest_pvalue)
 
